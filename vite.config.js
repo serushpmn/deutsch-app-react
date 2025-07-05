22b905acf60b39234a02d3d40efa74bbfa90a19b
unknown <soroushpey@gmail.com>
2025-07-05 12:44:47 +0330

    change Title

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// ⚠️ نام ریپازیتوری خود را اینجا وارد کنید (مثلاً dual-app)
+const repoName = 'deutsch-app-react'; // اگر نام ریپازیتوری شما چیز دیگری است، همینجا اصلاح کنید
+
 export default defineConfig({
+  base: `/${repoName}/`,
   plugins: [react()],
-})
+});
